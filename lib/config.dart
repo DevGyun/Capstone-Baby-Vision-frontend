4443b1762de7f2500f459df6e3317e4561d4f8ec
edwin200311 <129483212+edwin200311@users.noreply.github.com>
2026-04-11 15:48:07 +0900

    15:48

--- lib/config.dart
+++ lib/config.dart
@@ -3,13 +3,13 @@ import 'package:flutter/foundation.dart';
 
 class AppConfig {
   static String get baseUrl {
-    if (kIsWeb) return 'http://localhost:8000'; 
+    if (kIsWeb) return 'https://succeedable-untabled-dewitt.ngrok-free.dev'; 
     else if (Platform.isAndroid) return 'http://10.0.2.2:8000'; 
     else return 'http://localhost:8000'; 
   }
 
   static String get wsUrl {
-    if (kIsWeb) return 'ws://localhost:8000';
+    if (kIsWeb) return 'wss://succeedable-untabled-dewitt.ngrok-free.dev';
     else if (Platform.isAndroid) return 'ws://10.0.2.2:8000';
     else return 'ws://localhost:8000';
   }
@@ -17,7 +17,7 @@ class AppConfig {
   // 💡 새롭게 추가할 부분: RTSP 스트리밍 서버 주소
   static String get rtspUrl {
     if (kIsWeb) {
-      return 'rtsp://localhost:8554';
+      return 'rtsp://succeedable-untabled-dewitt.ngrok-free.dev';
     } else if (Platform.isAndroid) {
       return 'rtsp://10.0.2.2:8554';
     } else {
